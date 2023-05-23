031416d09e6a152276c5bec663713ac9bc7f8ebf
gjaltbearda <105846884+gjaltbearda@users.noreply.github.com>
2023-05-23 12:04:36 +0200

    flaskapp opstarten van nieuwe entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 service ssh start
-exec gunicorn -w 4 -b 0.0.0.0:8000 app:app
+FLASK_APP=/app/server.py flask run --host=0.0.0.0 --port=80
+#exec gunicorn -w 4 -b 0.0.0.0:8000 app:app
